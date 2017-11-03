fed3b790be6d2a91fdd5421e4076fe305ead831a
Ameya Joshi <ameya.woxi@gmail.com>
2017-11-03 13:53:42 +0000

    Asset Listing completed

--- app/Asset.php
+++ app/Asset.php
@@ -14,4 +14,10 @@ class Asset extends Model {
 
     protected $table = 'assets';
 
+    protected $fillable = ['name','model_number', 'expiry_date', 'price', 'is_fuel_dependent', 'litre_per_unit','is_active','asset_types_id', 'electricity_per_unit','quantity'];
+
+    public function assetTypes(){
+        return $this->belongsTo('App\AssetType','asset_types_id');
+    }
+
 }
