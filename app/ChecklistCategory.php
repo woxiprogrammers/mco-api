01590ee2a98c58d202c131c4b465053f2cee576f
harsha <harsha.woxi@gmail.com>
2017-11-21 15:40:10 +0530

    sluggable removed

--- app/ChecklistCategory.php
+++ app/ChecklistCategory.php
@@ -2,7 +2,6 @@
 
 namespace App;
 
-use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 
 class ChecklistCategory extends Model
@@ -10,14 +9,4 @@ class ChecklistCategory extends Model
     protected $table = 'checklist_categories';
 
     protected $fillable = ['name','slug','is_active','category_id'];
-
-    use Sluggable;
-    public function sluggable()
-    {
-        return [
-            'slug' => [
-                'source' => 'name'
-            ]
-        ];
-    }
 }
