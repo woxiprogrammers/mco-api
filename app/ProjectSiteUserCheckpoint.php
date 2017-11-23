36ab95a41952d72ab9fbe9bb768ba90d6bdc5ba8
harsha <harsha.woxi@gmail.com>
2017-11-23 18:34:42 +0530

    checkpoint API

--- app/ProjectSiteUserCheckpoint.php
+++ app/ProjectSiteUserCheckpoint.php
@@ -9,4 +9,8 @@ class ProjectSiteUserCheckpoint extends Model
     protected $table = 'project_site_user_checkpoints';
 
     protected $fillable = ['project_site_checklist_checkpoint_id','project_site_user_checkpoint_id','project_site_user_checklist_assignment_id','remark','is_ok'];
+
+    public function projectSiteChecklistCheckpoint(){
+        return $this->belongsTo('App\ProjectSiteChecklistCheckpoint','project_site_checklist_checkpoint_id');
+    }
 }
