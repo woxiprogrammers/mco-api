528edb73c300a0d17efefbec6f83414bb4a6c480
harsha <harsha.woxi@gmail.com>
2018-07-21 11:41:14 +0530

    done

--- app/Http/Controllers/Peticash/SalaryController.php
+++ app/Http/Controllers/Peticash/SalaryController.php
@@ -818,7 +818,9 @@ class SalaryController extends BaseController{
             $transactionTypeId = PeticashTransactionType::where('slug',$request['type'])->pluck('id')->first();
             $lastRequestedSalary = PeticashRequestedSalaryTransaction::where('employee_id',$request['employee_id'])
                 ->where('project_site_id',$request['project_site_id'])
-                ->where('peticash_transaction_type_id',$transactionTypeId)->select('amount','created_at')->get()->last();
+                ->where('peticash_status_id',PeticashStatus::where('slug','approved')->pluck('id')->first())
+                ->where('peticash_transaction_type_id',$transactionTypeId)
+                ->select('amount','created_at')->get()->last();
             if($lastRequestedSalary != null){
                 $salaryTransactionAmountAfterLastRequest = PeticashSalaryTransaction::where('created_at','>=',$lastRequestedSalary['created_at'])
                     ->where('employee_id',$request['employee_id'])
